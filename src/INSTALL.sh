20644e65d9ca964ec22e66685e4b22d35e9e76a4
Ka Wa Tsang <kwtsang@nikhef.nl>
2016-12-10 03:25:05 +0100

    Minor change on INSTALL.sh

--- src/INSTALL.sh
+++ src/INSTALL.sh
@@ -1,12 +1,14 @@
 # Add the following line to the .bashrc
+
+# loghunter
 export LogDirPath="/home/kwtsang/OneDrive_CUHK/mle/loghunter"
-export RelSrcPath="src"
+export LogRelSrcPath="src"
  
-alias loghunter="python ${LogDirPath}/${RelSrcPath}/loghunter.py"
+alias loghunter="python ${LogDirPath}/${LogRelSrcPath}/loghunter.py"
 alias logcombine="pdftk $(ls -l ${LogDirPath}/data/log/ | grep ^d | awk '{print $9}' | sed "s:^:${LogDirPath}/data/log/:g" | sed "s:$:/log.pdf:g" | tr '\n' ' ') cat output ${LogDirPath}/data/log/logcombine.pdf"
 alias logreport="okular ${LogDirPath}/data/log/logcombine.pdf"
 alias logcd="cd ${LogDirPath}/data/"
-alias logtodo="sh ${LogDirPath}/${RelSrcPath}/logtodo.sh"
+alias logtodo="sh ${LogDirPath}/${LogRelSrcPath}/logtodo.sh"
 
 # Ka Wa
 # 20161127
