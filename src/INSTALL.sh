1d20faef0ae569f6942a88597b80dd74eed2373c
Ka Wa Tsang <kwtsang@nikhef.nl>
2017-01-21 18:00:30 +0100

    Minor change on the src/INSTALL.sh.

--- src/INSTALL.sh
+++ src/INSTALL.sh
@@ -5,7 +5,7 @@ export LogDirPath="/home/kwtsang/OneDrive_CUHK/mle/loghunter"
 export LogRelSrcPath="src"
  
 alias loghunter="python ${LogDirPath}/${LogRelSrcPath}/loghunter.py"
-alias logcombine="pdftk $(ls -l ${LogDirPath}/data/log/ | grep ^d | awk '{print $9}' | sed "s:^:${LogDirPath}/data/log/:g" | sed "s:$:/log.pdf:g" | tr '\n' ' ') cat output ${LogDirPath}/data/log/logcombine.pdf"
+alias logcombine="pdftk `ls -l ${LogDirPath}/data/log/ | grep ^d | awk '{print $9}' | sed "s:^:${LogDirPath}/data/log/:g" | sed "s:$:/log.pdf:g" | tr '\n' ' '` cat output ${LogDirPath}/data/log/logcombine.pdf"
 alias logreport="okular ${LogDirPath}/data/log/logcombine.pdf"
 alias logcd="cd ${LogDirPath}/data/"
 alias logtodo="sh ${LogDirPath}/${LogRelSrcPath}/logtodo.sh"
